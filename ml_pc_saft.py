e4d24258e1c0c341d4acb5120275969f1dbb8933
Wildson Lima <wil_bbl@hotmail.com>
2023-06-10 20:56:36 -0300

    ok to revert

--- ml_pc_saft.py
+++ ml_pc_saft.py
@@ -1,6 +1,13 @@
 import jax.numpy as jnp
 import jax
-import epcsaft_complete
+from jax import dlpack as jdlpack
+from torch.utils import dlpack as tdlpack
+import epcsaft
+import torch
+
+from jax.config import config
+
+config.update("jax_enable_x64", True)
 
 
 def epcsaft_layer(parameters: jax.Array, state: jax.Array) -> jax.Array:
@@ -29,7 +36,7 @@ def epcsaft_layer(parameters: jax.Array, state: jax.Array) -> jax.Array:
 
     result = jax.lax.cond(
         fntype == 1,
-        epcsaft_complete.pcsaft_den,
+        epcsaft.pcsaft_den,
         gamma,
         x,
         m,
@@ -62,7 +69,7 @@ def gamma(
 ):
     x1 = (x < 0.5) * 1.0
 
-    rho = epcsaft_complete.pcsaft_den(
+    rho = epcsaft.pcsaft_den(
         x,
         m,
         s,
@@ -82,7 +89,7 @@ def gamma(
     )
 
     fungcoef = (
-        epcsaft_complete.pcsaft_fugcoef(
+        epcsaft.pcsaft_fugcoef(
             x,
             m,
             s,
@@ -102,7 +109,7 @@ def gamma(
         @ x1
     )
 
-    rho = epcsaft_complete.pcsaft_den(
+    rho = epcsaft.pcsaft_den(
         x1,
         m,
         s,
@@ -122,7 +129,7 @@ def gamma(
     )
 
     fungcoefpure = (
-        epcsaft_complete.pcsaft_fugcoef(
+        epcsaft.pcsaft_fugcoef(
             x1,
             m,
             s,
@@ -146,78 +153,89 @@ def gamma(
 
     return gamma1.squeeze()
 
-def epcsaft_pure_den(parameters: jax.Array, state: jax.Array) -> jax.Array:
-    x = jnp.asarray([[1.0]])
-    t = state[0]
-    p = state[1]
-    phase = state[2]
-    fntype = state[3]
 
-    m = parameters[0].reshape(1, 1)
-    s = parameters[1].reshape(1, 1)
-    e = parameters[2].reshape(1, 1)
-    vol_a = parameters[3].reshape(1, 1)
-    e_assoc = parameters[4].reshape(1, 1)
-    dipm = parameters[5].reshape(1, 1)
-    dip_num = parameters[6].reshape(1, 1)
+def VP(
+    x, m, s, e, t, p, k_ij, l_ij, khb_ij, e_assoc, vol_a, dipm, dip_num, z, dielc, phase
+):
+    return epcsaft.pcsaft_VP(
+        x, m, s, e, t, k_ij, l_ij, khb_ij, e_assoc, vol_a, dipm, dip_num, z, dielc
+    ).squeeze()
+
+
+class PCSAFT_layer(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input: torch.Tensor, state: torch.Tensor) -> torch.Tensor:
+        parameters = tdlpack.to_dlpack(input)
+        parameters = jdlpack.from_dlpack(parameters)
+
+        state = tdlpack.to_dlpack(state)
+        state = jdlpack.from_dlpack(state)
+
+        ctx.parameters = parameters
+        ctx.state = state
+        result = batch_pcsaft_layer(parameters, state)
+        result = jdlpack.to_dlpack(result)
+        result = tdlpack.from_dlpack(result)
+        return result
+
+    @staticmethod
+    def backward(ctx, dg1: torch.Tensor):
+        grad_result = grad_pcsaft_layer(ctx.parameters, ctx.state)
+        grad_result = jdlpack.to_dlpack(grad_result)
+        grad_result = tdlpack.from_dlpack(grad_result) * dg1[..., None]
+        grad_result = grad_result.nan_to_num(0, 0, 0)
+        return grad_result, None
+
+
+def epcsaft_layer_test(parameters: jax.Array, state: jax.Array) -> jax.Array:
+    x = jnp.asarray([[state[0]], [state[1]]])
+    t = state[2]
+    p = state[3]
+    phase = state[4]
+    fntype = state[5]
+
+    kij = parameters[-3:]
+    parameters = parameters[:-3].reshape(2, 7)
+
+    m = parameters[:, 0][..., jnp.newaxis]
+    s = parameters[:, 1][..., jnp.newaxis]
+    e = parameters[:, 2][..., jnp.newaxis]
+    vol_a = parameters[:, 3][..., jnp.newaxis]
+    e_assoc = parameters[:, 4][..., jnp.newaxis]
+    dipm = parameters[:, 5][..., jnp.newaxis]
+    dip_num = parameters[:, 6][..., jnp.newaxis]
     z = jnp.zeros_like(m)
     dielc = jnp.zeros_like(m)
 
-    k_ij = jnp.zeros_like(m)
-    l_ij = jnp.zeros_like(m)
-    khb_ij = jnp.zeros_like(m)
+    k_ij = jnp.asarray([[0.0, kij[0]], [kij[0], 0.0]])
+    l_ij = jnp.asarray([[0.0, kij[1]], [kij[1], 0.0]])
+    khb_ij = jnp.asarray([[0.0, kij[2]], [kij[2], 0.0]])
 
-    result = epcsaft_complete.pcsaft_den(
-        x,
-        m,
-        s,
-        e,
-        t,
-        p,
-        k_ij,
-        l_ij,
-        khb_ij,
-        e_assoc,
-        vol_a,
-        dipm,
-        dip_num,
-        z,
-        dielc,
-        phase,
+    result = epcsaft.pcsaft_VP(
+        x, m, s, e, t, k_ij, l_ij, khb_ij, e_assoc, vol_a, dipm, dip_num, z, dielc
     )
 
     return result.squeeze()
 
 
-batch_den = jax.jit(jax.vmap(epcsaft_pure_den, (None, 0)))
+batch_epcsaft_layer_test = jax.jit(jax.vmap(epcsaft_layer_test, (0, 0)))
 
 
-def epcsaft_pure_VP(parameters: jax.Array, state: jax.Array) -> jax.Array:
-    x = jnp.asarray([[1.0]])
-    t = state[0]
-    p = state[1]
-    phase = state[2]
-    fntype = state[3]
+class PCSAFT_layer_test(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input: torch.Tensor, state: torch.Tensor) -> torch.Tensor:
+        parameters = tdlpack.to_dlpack(input)
+        parameters = jdlpack.from_dlpack(parameters)
 
-    m = parameters[0].reshape(1, 1)
-    s = parameters[1].reshape(1, 1)
-    e = parameters[2].reshape(1, 1)
-    vol_a = parameters[3].reshape(1, 1)
-    e_assoc = parameters[4].reshape(1, 1)
-    dipm = parameters[5].reshape(1, 1)
-    dip_num = parameters[6].reshape(1, 1)
-    z = jnp.zeros_like(m)
-    dielc = jnp.zeros_like(m)
+        state = tdlpack.to_dlpack(state)
+        state = jdlpack.from_dlpack(state)
 
-    k_ij = jnp.zeros_like(m)
-    l_ij = jnp.zeros_like(m)
-    khb_ij = jnp.zeros_like(m)
+        result = batch_epcsaft_layer_test(parameters, state)
+        result = jdlpack.to_dlpack(result)
+        result = tdlpack.from_dlpack(result)
+        return result
 
-    result = epcsaft_complete.pcsaft_VP(
-        x, m, s, e, t, p, k_ij, l_ij, khb_ij, e_assoc, vol_a, dipm, dip_num, z, dielc
-    ).squeeze()
-
-    return result
-
-
-batch_VP = jax.jit(jax.vmap(epcsaft_pure_VP, (None, 0)))
+    @staticmethod
+    def backward(ctx, dg1):
+        grad_result = dg1[..., None]
+        return grad_result, None
