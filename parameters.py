4f07e8357487c215ab0aa0dbdbe2a4039a183b4a
Wildson Lima <wil_bbl@hotmail.com>
2023-06-28 10:19:01 -0300

    save at each iteration

--- parameters.py
+++ parameters.py
@@ -43,9 +43,7 @@ def fit_para():
     lossfn = MeanSquaredLogError().to(device)
 
     # Create the optimizer.
-    para = torch.tensor(
-        [1.52, 3.23, 188.9, 0.0351, 2899.5, 0.01, 1.0]
-    )
+    para = torch.tensor([1.52, 3.23, 188.9, 0.0351, 2899.5, 0.01, 1.0])
     para = para.to(device)
     para.requires_grad_()
     optimizer = torch.optim.SGD(
@@ -60,6 +58,7 @@ def fit_para():
     print("### starting iterations ###")
     if osp.exists("./data/thermoml/processed/parameters.pkl"):
         parameters = pickle.load(open("./data/thermoml/processed/parameters.pkl", "rb"))
+        print(f"inchis saved: {len(parameters.keys)}")
     else:
         parameters = {}
 
@@ -73,7 +72,7 @@ def fit_para():
         print(f"for inchi: {graphs.InChI[0]}")
         loss = 2
         step = 1
-        while (loss > 0.0001) & (step < 200):
+        while (loss > 0.0001) & (step < 100):
             optimizer.zero_grad()
             pred = pcsaft_layer(para.abs() + 1.0e-6, graphs.rho)
             loss = lossfn(pred, graphs.rho[:, -1])
@@ -84,17 +83,16 @@ def fit_para():
             optimizer.step()
             lr = scheduler.get_last_lr()[0]
             scheduler.step(step)
-            step += 1
             # Quick indication that training is happening.
             logging.log_first_n(logging.INFO, "Finished training step %d.", 10, step)
-            wandb.log({"train_msle": loss.item(),
-                      "lr": lr})
-        if (~loss.isnan()) & (loss <= 0.0001):
+            wandb.log({"train_msle": loss.item(), "lr": lr})
+            step += 1
+        if (~loss.isnan()):
             parameters[graphs.InChI[0]] = [para.tolist(), loss.item()]
-        print(f"params: {para}")
-    with open("./data/thermoml/processed/parameters.pkl", "wb") as file:
-        # A new file will be created
-        pickle.dump(parameters, file)
+            print(f"params: {para}")
+        with open("./data/thermoml/processed/parameters.pkl", "wb") as file:
+            # A new file will be created
+            pickle.dump(parameters, file)
     wandb.finish()
 
 
