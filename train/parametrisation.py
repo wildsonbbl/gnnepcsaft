4d5795ee391f122a98d513ad2691b02520f54f85
wildsonbbl <wil_bbl@hotmail.com>
2023-08-18 01:50:37 +0000

    added log mape

--- train/parametrisation.py
+++ train/parametrisation.py
@@ -21,17 +21,52 @@ device = torch.device("cpu")
 with open("./data/thermoml/processed/para3.pkl", "rb") as file:
     init_para = pickle.load(file)
 
-def parametrisation(gamma):
 
+def MAPE(parameters: np.ndarray, rho: np.ndarray, vp: np.ndarray):
+    parameters = np.abs(parameters)
+    m = parameters[0]
+    s = parameters[1]
+    e = parameters[2]
+    mape = []
+
+    if ~np.all(rho == np.zeros_like(rho)):
+        for state in rho:
+            x = np.asarray([1.0])
+            t = state[0]
+            p = state[1]
+            phase = ["liq" if state[2] == 1 else "vap"][0]
+            params = {"m": m, "s": s, "e": e}
+            den = pcsaft_den(t, p, x, params, phase=phase)
+            mape += [np.abs((state[-1] - den) / state[-1])]
+
+    if ~np.all(vp == np.zeros_like(vp)):
+        for state in vp:
+            x = np.asarray([1.0])
+            t = state[0]
+            p = state[1]
+            phase = ["liq" if state[2] == 1 else "vap"][0]
+            params = {"m": m, "s": s, "e": e}
+            try:
+                vp, xl, xv = flashTQ(t, 0, x, params, p)
+                mape += [np.abs((state[-1] - vp) / state[-1])]
+            except:
+                continue
+
+    mape = np.asarray(mape).mean()
+
+    return mape
+
+
+def parametrisation(weight_decay):
     def loss(parameters: np.ndarray, rho: np.ndarray, vp: np.ndarray):
         parameters = np.abs(parameters)
         m = parameters[0]
         s = parameters[1]
         e = parameters[2]
         loss = []
-        
+
         n = rho.shape[0] + vp.shape[0]
-        weight_decay = np.sum(parameters**2) * gamma / n
+        l2penalty = np.sum(parameters**2) * weight_decay / n
 
         if ~np.all(rho == np.zeros_like(rho)):
             for state in rho:
@@ -41,7 +76,7 @@ def parametrisation(gamma):
                 phase = ["liq" if state[2] == 1 else "vap"][0]
                 params = {"m": m, "s": s, "e": e}
                 den = pcsaft_den(t, p, x, params, phase=phase)
-                loss += [((state[-1] - den) / state[-1]) * np.sqrt(3) + weight_decay]
+                loss += [((state[-1] - den) / state[-1]) * np.sqrt(3) + l2penalty]
 
         if ~np.all(vp == np.zeros_like(vp)):
             for state in vp:
@@ -52,10 +87,10 @@ def parametrisation(gamma):
                 params = {"m": m, "s": s, "e": e}
                 try:
                     vp, xl, xv = flashTQ(t, 0, x, params, p)
-                    loss += [((state[-1] - vp) / state[-1]) * np.sqrt(2) + weight_decay]
+                    loss += [((state[-1] - vp) / state[-1]) * np.sqrt(2) + l2penalty]
                 except:
-                    loss += [weight_decay]
-                
+                    loss += [l2penalty]
+
         loss = np.asarray(loss).flatten()
 
         return loss
@@ -63,9 +98,7 @@ def parametrisation(gamma):
     run = wandb.init(
         # Set the project where this run will be logged
         project="gnn-pc-saft",
-        config = {
-            "weight decay": gamma
-        }
+        config={"weight decay": weight_decay},
     )
     fitted_para = {}
 
@@ -79,18 +112,21 @@ def parametrisation(gamma):
             n_skipped += 1
             continue
         params = np.asarray(init_para[graph.InChI[0]][0])
-        res = least_squares(loss, params, method="lm", x_scale = x_scale, args=(rho, vp))
+        res = least_squares(loss, params, method="lm", x_scale=x_scale, args=(rho, vp))
         fit_para = np.abs(res.x).tolist()
         cost = res.cost
+        mape = MAPE(res.x, rho, vp)
         wandb.log(
-                    {
-                        "cost": cost,
-                        "m": fit_para[0],
-                        "s": fit_para[1],
-                        "e": fit_para[2]
-                    },
+            {
+                "cost": cost,
+                "m": fit_para[0],
+                "s": fit_para[1],
+                "e": fit_para[2],
+                "mape": mape,
+            },
         )
-        fitted_para[graph.InChI[0]] = (fit_para, cost)
+        if mape < 5 / 100:
+            fitted_para[graph.InChI[0]] = (fit_para, mape)
         with open("./data/thermoml/raw/para3_fitted.pkl", "wb") as file:
             pickle.dump(fitted_para, file)
     print(
@@ -99,10 +135,10 @@ def parametrisation(gamma):
     wandb.finish()
 
 
-
 FLAGS = flags.FLAGS
 
-flags.DEFINE_float("weight_decay", None, "L2 penalty.")
+flags.DEFINE_float("weight_decay", None, "For L2 penalty.")
+
 
 def main(argv):
     if len(argv) > 1:
