81e9735d68edc2850b1d83e1943c4082ca7e6460
wildsonbbl <wil_bbl@hotmail.com>
2023-10-17 12:18:43 +0000

    model corrections

--- demo/utils.py
+++ demo/utils.py
@@ -2,7 +2,7 @@
 import os.path as osp, os
 os.environ["CUDA_VISIBLE_DEVICES"] = ''
 import torch, numpy as np
-from data.graphdataset import ThermoMLDataset
+from data.graphdataset import ThermoMLDataset, ramirez, ThermoMLpara
 from train.models import PNAPCSAFT
 from train.model_deg import calc_deg
 from train.parametrisation import rhovp_data
@@ -14,6 +14,20 @@ model_dtype = torch.float64
 deg_model2 = calc_deg("thermoml", './')
 deg_model1 = calc_deg("ramirez", './')
 
+ra_loader = ramirez("./data/ramirez2022")
+ra_para = {}
+for graph in ra_loader:
+    inchi, para = graph.InChI, graph.para.view(-1, 3).round(decimals=2)
+    ra_para[inchi] = para.tolist()[0]
+tml_loader = ThermoMLpara("./data/thermoml")
+tml_para = {}
+for graph in tml_loader:
+    inchi, para = graph.InChI, graph.para.view(-1, 3).round(decimals=2)
+    tml_para[inchi] = para.tolist()[0]
+path = osp.join("data", "thermoml")
+testloader = ThermoMLDataset(path)
+device = torch.device("cpu")
+
 
 def loadckp(ckp_path: str, model: PNAPCSAFT):
     if osp.exists(ckp_path):
@@ -22,7 +36,7 @@ def loadckp(ckp_path: str, model: PNAPCSAFT):
         print(f"model checkpoint step {checkpoint['step']}")
         del checkpoint
 
-def plotdata(inchi: str, model_name: str, molecule_name: str, model: PNAPCSAFT, testloader: ThermoMLDataset, ra_para: dict):
+def plotdata(inchi: str, molecule_name: str, models: list[PNAPCSAFT]):
     def pltline(x, y):
         return plt.plot(x, y, linewidth=0.5)
 
@@ -35,9 +49,9 @@ def plotdata(inchi: str, model_name: str, molecule_name: str, model: PNAPCSAFT, 
             ta = x[i]
             if (mape > 1) & (ta - tb > 2):
                 tb = ta
-                plt.text(x[i], y[i], f"{mape} %", ha="center", va="center", fontsize=4)
+                plt.text(x[i], y[i], f"{mape} %", ha="center", va="center", fontsize=8)
 
-    model.eval()
+    
     with torch.no_grad():
         for graphs in testloader:
             if inchi == graphs.InChI:
@@ -45,84 +59,92 @@ def plotdata(inchi: str, model_name: str, molecule_name: str, model: PNAPCSAFT, 
         graphs.x = graphs.x.to(model_dtype)
         graphs.edge_attr = graphs.edge_attr.to(model_dtype)
         graphs.edge_index = graphs.edge_index.to(torch.int64)
-
         graphs = graphs.to(device)
-        parameters = model(graphs)
-        params = parameters.squeeze().to(torch.float64).numpy()
-        # params[0] -= 0.0112
-        rho = graphs.rho.view(-1, 5).to(torch.float64).numpy()
-        vp = graphs.vp.view(-1, 5).to(torch.float64).numpy()
+        list_params = []
+        for model in models:
+            model.eval()
+            parameters = model(graphs)
+            params = parameters.squeeze().to(torch.float64).numpy()
+            list_params.append(params)
+
+    rho = graphs.rho.view(-1, 5).to(torch.float64).numpy()
+    vp = graphs.vp.view(-1, 5).to(torch.float64).numpy()
+    pred_den_list, pred_vp_list  = [], []
+    for params in list_params:
         pred_den, pred_vp = rhovp_data(params, rho, vp)
+        pred_den_list.append(pred_den)
+        pred_vp_list.append(pred_vp)
+    if inchi in ra_para:
         params = np.asarray(ra_para[inchi])
         ra_den, ra_vp = rhovp_data(params, rho, vp)
 
-        idx_vp = (pred_vp != 0) & (ra_vp != 0)
-        vp = vp[idx_vp]
-        ra_vp = ra_vp[idx_vp]
-        pred_vp = pred_vp[idx_vp]
-
+    if ~np.all(vp == np.zeros_like(vp)):
         idx = np.argsort(vp[:, 0], 0)
         x = vp[idx, 0]
         y = vp[idx, -1] / 100000
         pltscatter(x, y)
 
-        y = pred_vp[idx] / 100000
-        pltline(x, y)
-        mvp_model = 100 * np.abs(vp[idx, -1] - pred_vp[idx]) / vp[idx, -1]
-        plterr(x, y * 0.99, mvp_model)
+        for pred_vp in pred_vp_list:    
+            y = pred_vp[idx] / 100000
+            pltline(x, y)
+            mvp_model = 100 * np.abs(vp[idx, -1] - pred_vp[idx]) / vp[idx, -1]
+            #plterr(x, y, mvp_model)
 
-        y = ra_vp[idx] / 100000
-        pltline(x, y)
-        mvp_ra = 100 * np.abs(vp[idx, -1] - ra_vp[idx]) / vp[idx, -1]
-        plterr(x, y * 1.01, mvp_ra)
+        if inchi in ra_para:
+            ra_vp = ra_vp
+            y = ra_vp[idx] / 100000
+            pltline(x, y)
+            mvp_ra = 100 * np.abs(vp[idx, -1] - ra_vp[idx]) / vp[idx, -1]
+            #plterr(x, y * 1.01, mvp_ra)
 
         # Customize the plot appearance
-        pltcustom(model_name, mvp_model, mvp_ra, "linear", "Pressão de vapor (bar)")
+        pltcustom(inchi in ra_para, "log", "Pressão de vapor (bar)")
 
         # Save the plot as a high-quality image file
-        path = osp.join(
-            "images", "vp_" + model_name.strip() + "_" + molecule_name + ".png"
-        )
-        plt.savefig(path, dpi=300)
+        path = osp.join("images", "vp_" + molecule_name + ".png")
         plt.show()
 
-        idx_p = abs(rho[:, 1] - 101325) < 10000
+    if ~np.all(rho == np.zeros_like(rho)):
+        idx_p = abs(rho[:, 1] - 101325) < 15_000
         rho = rho[idx_p]
-        pred_den = pred_den[idx_p]
-        ra_den = ra_den[idx_p]
         idx = np.argsort(rho[:, 0], 0)
-
         x = rho[idx, 0]
         y = rho[idx, -1]
         pltscatter(x, y)
 
-        y = pred_den[idx]
-        pltline(x, y)
-        mden_model = 100 * np.abs(rho[idx, -1] - pred_den[idx]) / rho[idx, -1]
-        plterr(x, y, mden_model)
+        for pred_den in pred_den_list:
+            pred_den = pred_den[idx_p]
+            y = pred_den[idx]
+            pltline(x, y)
+            mden_model = 100 * np.abs(rho[idx, -1] - pred_den[idx]) / rho[idx, -1]
+            #plterr(x, y, mden_model)    
 
-        y = ra_den[idx]
-        pltline(x, y)
-        mden_ra = 100 * np.abs(rho[idx, -1] - ra_den[idx]) / rho[idx, -1]
-        plterr(x, y, mden_ra)
+        if inchi in ra_para:
+            ra_den = ra_den[idx_p]
+            y = ra_den[idx]
+            pltline(x, y)
+            mden_ra = 100 * np.abs(rho[idx, -1] - ra_den[idx]) / rho[idx, -1]
+            #plterr(x, y, mden_ra)
 
         # Customize the plot appearance
-        pltcustom(model_name, mden_model, mden_ra, "linear", "Densidade (mol / m³)")
-        path = osp.join("images", "den_" + model_name.strip()+ "_" + molecule_name + ".png")
-        plt.savefig(path, dpi=300)
+        pltcustom(inchi in ra_para, "linear", "Densidade (mol / m³)")
+        path = osp.join(
+            "images", "den_" + molecule_name + ".png"
+        )
         plt.show()
 
 
-def pltcustom(modelname, m_model, m_ra, scale="linear", ylabel=""):
+def pltcustom(ra, scale="linear", ylabel=""):
     plt.xlabel("T (K)")
     plt.ylabel(ylabel)
     plt.title("")
-    plt.legend(
-        [
-            "ThermoML",
-            modelname + f" ({m_model.mean().round(decimals=2)} %)",
-            f"Ramírez-Vélez et al. ({m_ra.mean().round(decimals=2)} %)",
-        ]
-    )
+    legend = [
+        "Pontos experimentais",
+        "Modelo 1",
+        "Modelo 2"
+    ]
+    if ra:
+        legend += [f"Ramírez-Vélez et al. (2022)"]
+    plt.legend(legend, loc = (1.01,0.75))
     plt.grid(False)
     plt.yscale(scale)
