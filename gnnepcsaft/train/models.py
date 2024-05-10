327a14612688221a189929f412aa247ba4fe71b9
wildsonbbl <wil_bbl@hotmail.com>
2024-05-10 20:56:20 +0000

    0.1 mape weight for vp < 10000

--- gnnepcsaft/train/models.py
+++ gnnepcsaft/train/models.py
@@ -233,6 +233,8 @@ class PNApcsaftL(L.LightningModule):
             result_filter = ~torch.isnan(pred)
             # pylint: disable = not-callable
             loss_mape = mape(pred[result_filter], target[result_filter])
+            if torch.max(target).item() < 10000:
+                loss_mape *= 0.1
             loss_huber = hloss(pred[result_filter], target[result_filter])
             if loss_mape.item() < 0.9:
                 mape_vp = loss_mape.item()
