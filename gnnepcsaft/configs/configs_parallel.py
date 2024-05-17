49cdd44628abfd6a5873bc17ccfdca91c680b65d
wildsonbbl <wil_bbl@hotmail.com>
2024-05-17 20:19:01 +0000

    selected configs

--- gnnepcsaft/configs/configs_parallel.py
+++ gnnepcsaft/configs/configs_parallel.py
@@ -14,13 +14,13 @@ def get_configs() -> dict:
             "add_self_loops": True,
         },
         {
-            "propagation_depth": 3,
-            "hidden_dim": 64,
-            "num_mlp_layers": 0,
-            "pre_layers": 2,
-            "post_layers": 1,
+            "propagation_depth": 6,
+            "hidden_dim": 256,
+            "num_mlp_layers": 1,
+            "pre_layers": 1,
+            "post_layers": 2,
             "skip_connections": True,
-            "add_self_loops": True,
+            "add_self_loops": False,
         },
     ]
     return configs
