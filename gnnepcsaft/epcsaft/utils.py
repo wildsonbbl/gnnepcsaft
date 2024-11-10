d111bdf933bcbbb5e37b8c2e7902d1627fd67715
wildsonbbl <wil_bbl@hotmail.com>
2024-11-10 20:07:42 +0000

    pylint fixes

--- gnnepcsaft/epcsaft/utils.py
+++ gnnepcsaft/epcsaft/utils.py
@@ -15,26 +15,22 @@ def pure_den_teqp(parameters: np.ndarray, state: np.ndarray) -> np.ndarray:
 
     t = state[0]  # Temperature, K
 
-    m = max(parameters[0], 1.0)  # units
-    s = parameters[1]  # Å
-    e = parameters[2]  # K
     # pylint: disable=E1101,I1101
     c = teqp.SAFTCoeffs()
-    c.m = m
-    c.sigma_Angstrom = s
-    c.epsilon_over_k = e
-    coeffs = [c]
-    model = teqp.PCSAFTEOS(coeffs)
+    c.m = max(parameters[0], 1.0)  # units
+    c.sigma_Angstrom = parameters[1]  # Å
+    c.epsilon_over_k = parameters[2]  # K
+    model = teqp.PCSAFTEOS([c])
 
     # pylint: disable=I1101
     # T tilde = T / (e / kB) https://teqp.readthedocs.io/en/latest/models/PCSAFT.html
-    [ttilde_crit, ttilde_min] = PCSAFTsuperanc.get_Ttilde_crit_min(m=m)
-    ttilde = t / e
+    [ttilde_crit, ttilde_min] = PCSAFTsuperanc.get_Ttilde_crit_min(m=c.m)
+    ttilde = t / c.epsilon_over_k
     ttilde = min(max(ttilde, ttilde_min), ttilde_crit)
     # Rho tilde = RhoN * sigma ** 3 https://teqp.readthedocs.io/en/latest/models/PCSAFT.html
-    [tilderhol, tilderhov] = PCSAFTsuperanc.PCSAFTsuperanc_rhoLV(Ttilde=ttilde, m=m)
+    [tilderhol, tilderhov] = PCSAFTsuperanc.PCSAFTsuperanc_rhoLV(Ttilde=ttilde, m=c.m)
     rhol_guess, rhov_guess = [
-        tilderho / (N_A * s**3) for tilderho in [tilderhol, tilderhov]
+        tilderho / (N_A * c.sigma_Angstrom**3) for tilderho in [tilderhol, tilderhov]
     ]
 
     rhol, rhov = model.pure_VLE_T(t, rhol_guess * 0.98, rhov_guess * 1.02, 10)
