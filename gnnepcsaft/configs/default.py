3a8c700c9e1030edc96cc4e2cca40c8a9c873137
wildsonbbl <wil_bbl@hotmail.com>
2024-12-16 17:22:16 +0000

    new run

--- gnnepcsaft/configs/default.py
+++ gnnepcsaft/configs/default.py
@@ -9,38 +9,37 @@ def get_config():
     config.job_type = "train"
 
     # Optimizer.
-    config.optimizer = "sgd"
-    config.learning_rate = 1.0e-9
-    config.change_opt = True
+    config.optimizer = "adam"
+    config.learning_rate = 1e-3
+    config.change_opt = False
     config.change_sch = False
+    config.weight_decay = 1e-2
+    config.momentum = 0.9
+    config.patience = 5
+    config.warmup_steps = 100
 
     # Training hyperparameters.
 
     config.accelerator = "gpu"
     config.batch_size = 512
     config.pad_size = 128
-    config.num_train_steps = 250_000
-    config.warmup_steps = 100
-    config.log_every_steps = 1_000
+    config.num_train_steps = 150_000
+    config.log_every_steps = 5_000
     config.eval_every_steps = 24_999
     config.checkpoint_every_steps = 25_000
-    config.amp = False
-    config.momentum = 0.9
-    config.patience = 5
-    config.weight_decay = 1e-2
     config.dataset = "esper"
-    config.checkpoint = "esper_msigmae_2-epoch=36999-train_mape=0.0056.ckpt"
+    config.checkpoint = ""
 
     # GNN hyperparameters.
-    config.model_name = "esper_msigmae_2.1"
+    config.model_name = "esper_msigmae_2"
     config.model = "PNAL"
     config.propagation_depth = 6
     config.hidden_dim = 64
     config.pre_layers = 2
     config.post_layers = 2
-    config.dropout_rate = 0.0
     config.num_mlp_layers = 2
     config.num_para = 3
     config.skip_connections = False
     config.add_self_loops = False
+    config.dropout_rate = 0.0
     return config
