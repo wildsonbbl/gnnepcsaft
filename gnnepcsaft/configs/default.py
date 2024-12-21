49266e9fa5e086a8893981dbc8cbc85bef7e5d6f
wildsonbbl <wil_bbl@hotmail.com>
2024-12-21 20:09:59 +0000

    new run

--- gnnepcsaft/configs/default.py
+++ gnnepcsaft/configs/default.py
@@ -22,7 +22,7 @@ def get_config():
 
     config.accelerator = "gpu"
     config.batch_size = 1842
-    config.num_train_steps = 225_000
+    config.num_train_steps = 25_000
     config.log_every_steps = 100
     config.eval_every_steps = 100
     config.checkpoint_every_steps = 101
