22fd0cef8454c5a590ac38280b3ae320b4e8da67
wildsonbbl <wil_bbl@hotmail.com>
2025-01-09 13:56:54 +0000

    new run

--- gnnepcsaft/configs/configs_parallel.py
+++ gnnepcsaft/configs/configs_parallel.py
@@ -5,24 +5,17 @@ def get_configs() -> dict:
     """Get the hyperparameter configurations."""
     configs = [
         {
-            "propagation_depth": 6,
+            "propagation_depth": 7,
             "hidden_dim": 64,
-            "pre_layers": 1,
-            "post_layers": 1,
-            "add_self_loops": True,
-        },
-        {
-            "propagation_depth": 6,
-            "hidden_dim": 64,
+            "post_layers": 3,
             "pre_layers": 2,
-            "post_layers": 2,
             "add_self_loops": True,
         },
         {
-            "propagation_depth": 6,
+            "propagation_depth": 7,
             "hidden_dim": 64,
-            "pre_layers": 1,
-            "post_layers": 1,
+            "post_layers": 3,
+            "pre_layers": 2,
             "add_self_loops": True,
         },
     ]
