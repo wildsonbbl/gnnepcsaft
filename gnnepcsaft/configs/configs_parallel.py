22cb18878c5ca874036fa88a17e513ce14387e09
wildsonbbl <wil_bbl@hotmail.com>
2025-02-08 17:48:47 +0000

    new run

--- gnnepcsaft/configs/configs_parallel.py
+++ gnnepcsaft/configs/configs_parallel.py
@@ -1,22 +1,26 @@
 """Module to get configs for model hyperparameters in parallel training"""
 
 
-def get_configs() -> dict:
+def get_configs() -> list[dict]:
     """Get the hyperparameter configurations."""
     configs = [
         {
+            "model_name": "esper_msigmae_7.1",
+            "model": "GATL",
             "propagation_depth": 7,
-            "hidden_dim": 64,
-            "post_layers": 3,
-            "pre_layers": 2,
-            "add_self_loops": True,
+            "hidden_dim": 256,
+            "post_layers": None,
+            "pre_layers": None,
+            "heads": 3,
         },
         {
-            "propagation_depth": 7,
-            "hidden_dim": 64,
-            "post_layers": 3,
-            "pre_layers": 2,
-            "add_self_loops": True,
+            "model_name": "esper_msigmae_5.3",
+            "model": "PNAL",
+            "propagation_depth": 6,
+            "hidden_dim": 128,
+            "post_layers": 1,
+            "pre_layers": 3,
+            "heads": None,
         },
     ]
     return configs
