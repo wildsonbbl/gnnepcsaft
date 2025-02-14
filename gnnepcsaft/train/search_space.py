32d5b23207e886ecf0df5fb92016268f9cb2e05d
wildsonbbl <wil_bbl@hotmail.com>
2025-02-14 00:28:44 +0000

    refatora espaço de busca de hiperparâmetros para usar amostragem aleatória

--- gnnepcsaft/train/search_space.py
+++ gnnepcsaft/train/search_space.py
@@ -1,5 +1,6 @@
 """Module for hyperparameter search space definition for tuning"""
 
+import numpy as np
 from ray import tune
 
 
@@ -7,9 +8,13 @@ from ray import tune
 def get_search_space():
     """Hyperparameter search space for tuning"""
     return {
-        "propagation_depth": tune.choice([3, 4, 5, 6, 7, 8]),
-        "hidden_dim": tune.choice([64, 128, 256]),
-        "pre_layers": tune.choice([1]),
-        "post_layers": tune.choice([1]),
-        "heads": tune.choice([1, 2, 3]),
+        "propagation_depth": tune.sample_from(
+            lambda spec: np.random.choice([3, 4, 5, 6, 7, 8]).item()
+        ),
+        "hidden_dim": tune.sample_from(
+            lambda spec: np.random.choice([64, 128, 256]).item()
+        ),
+        "pre_layers": tune.sample_from(lambda spec: np.random.choice([1]).item()),
+        "post_layers": tune.sample_from(lambda spec: np.random.choice([1]).item()),
+        "heads": tune.sample_from(lambda spec: np.random.choice([1, 2, 3]).item()),
     }
