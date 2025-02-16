aa3b49de2f5cf331f4d4c6cfbb7b8847b128de29
wildsonbbl <wil_bbl@hotmail.com>
2025-02-16 23:09:43 +0000

    bohb + wandb logger

--- gnnepcsaft/train/xgb_tuner.py
+++ gnnepcsaft/train/xgb_tuner.py
@@ -2,6 +2,10 @@
 
 from absl import app, flags, logging
 from ray import train, tune
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.tune.schedulers import HyperBandForBOHB
+from ray.tune.search import ConcurrencyLimiter
+from ray.tune.search.bohb import TuneBOHB
 
 from .xgb_training import training
 
@@ -14,8 +18,8 @@ def main(argv):
         raise app.UsageError("Too many command-line arguments.")
 
     logging.info("Calling tuner!")
-    # Define the search space
-    search_config = {
+    # Hyperparameter search space
+    search_space = {
         "eta": tune.loguniform(1e-4, 1e-1),
         "lambda": tune.loguniform(1e-6, 1e-1),
         "alpha": tune.loguniform(1e-6, 1e-1),
@@ -23,9 +27,34 @@ def main(argv):
     }
 
     workdir = FLAGS.workdir
+    # BOHB search algorithm
+    search_alg = TuneBOHB(metric="mape_den", mode="min", seed=77, max_concurrent=2)
+    search_alg = ConcurrencyLimiter(search_alg, max_concurrent=2)
+    # Early stopping scheduler for BOHB
+    scheduler = HyperBandForBOHB(
+        metric="mape_den",
+        mode="min",
+        max_t=1,
+        stop_last_trials=True,
+    )
+
+    # tune run config with wandb logger
+    run_config = train.RunConfig(
+        name="gnnpcsaft",
+        storage_path=None,
+        callbacks=(
+            [
+                WandbLoggerCallback(
+                    "gnn-pc-saft",
+                    "xgb",
+                    tags=["tuning", "xgb", "esper"],
+                )
+            ]
+        ),
+    )
 
     def tune_xgb(config: dict):
-        """Tune the xgb model"""
+        """trainable fun for the xgb model"""
         mape_den, mape_vp = training(workdir, config)
 
         train.report({"mape_den": mape_den, "mape_vp": mape_vp, "done": True})
@@ -33,8 +62,11 @@ def main(argv):
     # Run the tuner
     tuner = tune.Tuner(
         tune.with_resources(tune_xgb, resources={"cpu": 2, "gpu": 0.5}),
-        tune_config=tune.TuneConfig(num_samples=30),
-        param_space=search_config,
+        param_space=search_space,
+        tune_config=tune.TuneConfig(
+            num_samples=60, search_alg=search_alg, scheduler=scheduler
+        ),
+        run_config=run_config,
     )
     result = tuner.fit()
     best_trial = result.get_best_result(
