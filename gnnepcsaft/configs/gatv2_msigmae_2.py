f0b5e017ad947ba20cefafd27ab56e4591828243
wildsonbbl <wil_bbl@hotmail.com>
2025-04-10 21:57:17 -0300

    new run

--- gnnepcsaft/configs/gatv2_msigmae_2.py
+++ gnnepcsaft/configs/gatv2_msigmae_2.py
@@ -22,11 +22,11 @@ def get_config():
 
     config.accelerator = "auto"
     config.batch_size = 512
-    config.num_train_steps = 300_000
+    config.num_train_steps = 400_000
     config.log_every_steps = 1000
     config.eval_every_steps = 2500
     config.dataset = "esper"
-    config.checkpoint = "wildson/gnn-pc-saft/model-tgl0g9n3:v2"  # wandb artifact path
+    config.checkpoint = "wildson/gnn-pc-saft/model-bf66sl7s:v0"  # wandb artifact path
     config.resume_id = ""  # wandb run id
     config.model = "gnn"
     config.model_name = "gatv2_msigmae_1.1"
