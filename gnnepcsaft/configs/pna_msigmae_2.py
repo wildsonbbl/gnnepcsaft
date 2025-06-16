5e8bf18038140cf228bbc284f560bc5860e28335
wildsonbbl <wil_bbl@hotmail.com>
2025-06-16 12:14:01 -0300

    new run

--- gnnepcsaft/configs/pna_msigmae_2.py
+++ gnnepcsaft/configs/pna_msigmae_2.py
@@ -22,11 +22,11 @@ def get_config():
 
     config.accelerator = "auto"
     config.batch_size = 512
-    config.num_train_steps = 2_700_000
+    config.num_train_steps = 2_800_000
     config.log_every_steps = 1000
     config.eval_every_steps = 2500
     config.dataset = "esper"
-    config.checkpoint = "wildson/gnn-pc-saft/model-1dk4fpzp:v0"  # wandb artifact path
+    config.checkpoint = "wildson/gnn-pc-saft/model-cef6p9al:v0"  # wandb artifact path
     config.resume_id = ""  # wandb run id
     config.model = "gnn"
     config.model_name = "pna_msigmae_1.1"
--- gnnepcsaft/configs/transformer_msigmae_2.py
+++ gnnepcsaft/configs/transformer_msigmae_2.py
@@ -22,11 +22,11 @@ def get_config():
 
     config.accelerator = "auto"
     config.batch_size = 512
-    config.num_train_steps = 1_400_000
+    config.num_train_steps = 1_500_000
     config.log_every_steps = 1000
     config.eval_every_steps = 2500
     config.dataset = "esper"
-    config.checkpoint = "wildson/gnn-pc-saft/model-r9ekq68s:v0"  # wandb artifact path
+    config.checkpoint = "wildson/gnn-pc-saft/model-5i2hrnwb:v0"  # wandb artifact path
     config.resume_id = ""  # wandb run id
     config.model = "gnn"
     config.model_name = "transformer_msigmae_2.0"
