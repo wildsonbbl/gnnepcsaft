7ec3436132b01afbd609cacb842eb60bfdddd26d
wildsonbbl <wil_bbl@hotmail.com>
2025-09-25 00:22:07 -0300

    fix doc name

--- gnnepcsaft/epcsaft/utils.py
+++ gnnepcsaft/epcsaft/utils.py
@@ -1,4 +1,4 @@
-"""Module for ePC-SAFT calculations."""
+"""Module for PC-SAFT calculations."""
 
 from .epcsaft_feos import *  # pylint: disable=wildcard-import,unused-wildcard-import
 from .epcsaft_teqp import *  # pylint: disable=wildcard-import,unused-wildcard-import
