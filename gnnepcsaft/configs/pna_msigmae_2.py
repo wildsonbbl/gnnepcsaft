75c989b50e3d5952fd83efd40f9e2070fb2c816c
wildsonbbl <wil_bbl@hotmail.com>
2026-01-28 17:55:21 -0300

    new run

--- gnnepcsaft/configs/pna_msigmae_2.py
+++ gnnepcsaft/configs/pna_msigmae_2.py
@@ -22,11 +22,11 @@ def get_config():
 
     config.accelerator = "auto"
     config.batch_size = 512
-    config.num_train_steps = 4_000_000
+    config.num_train_steps = 4_200_000
     config.log_every_steps = 500
     config.eval_every_steps = 2500
     config.dataset = "esper"
-    config.checkpoint = "wildson/gnn-pc-saft/model-xkkxh5rw:v0"  # wandb artifact path
+    config.checkpoint = "wildson/gnn-pc-saft/model-cwsgrvqy:v0"  # wandb artifact path
     config.resume_id = ""  # wandb run id
     config.model = "gnn"
     config.model_name = "pna_msigmae_1.1"
