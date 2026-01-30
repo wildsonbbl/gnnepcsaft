ae9772fdcfffc3a5db53b0e4ad312a1c1b16c1c0
wildsonbbl <wil_bbl@hotmail.com>
2026-01-29 23:13:42 -0300

    add more convs

--- gnnepcsaft/configs/search_space.py
+++ gnnepcsaft/configs/search_space.py
@@ -12,7 +12,21 @@ def get_search_space() -> CS.ConfigurationSpace:
     b = CS.CategoricalHyperparameter("hidden_dim", [128, 256, 512], 256)
     c = CS.Constant("dropout", 0.0)
     d = CS.Constant("global_pool", "add")
-    e = CS.CategoricalHyperparameter("conv", ["PNA", "GATv2", "Transformer"], "PNA")
+    e = CS.CategoricalHyperparameter(
+        "conv",
+        [
+            "PNA",
+            "GATv2",
+            "Transformer",
+            "GCN",
+            "SAGE",
+            "GIN",
+            "GINE",
+            "Graph",
+            "SG",
+        ],
+        "PNA",
+    )
     f = CS.CategoricalHyperparameter("heads", [1, 2, 4, 8], 2)
     g = CS.UniformIntegerHyperparameter("post_layers", 1, 4, 2)
     h = CS.UniformIntegerHyperparameter("pre_layers", 1, 4, 3)
