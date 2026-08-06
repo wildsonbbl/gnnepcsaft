aabed3744ac56dbc85cfbfd8f25b9d87a8c640d9
wildsonbbl <wil_bbl@hotmail.com>
2026-08-06 19:57:23 -0300

    new search space

--- gnnepcsaft/configs/search_space.py
+++ gnnepcsaft/configs/search_space.py
@@ -8,9 +8,9 @@ def get_search_space() -> CS.ConfigurationSpace:
     """Hyperparameter search space for tuning"""
     search_space = CS.ConfigurationSpace()
 
-    a = CS.UniformIntegerHyperparameter("propagation_depth", 3, 8, 6)
-    b = CS.CategoricalHyperparameter("hidden_dim", [128, 256, 512], 256)
-    c = CS.UniformFloatHyperparameter("dropout", 0.0, 0.75, 0.0)
+    a = CS.UniformIntegerHyperparameter("propagation_depth", 1, 8, 6)
+    b = CS.CategoricalHyperparameter("hidden_dim", [16, 32, 64, 128, 256, 512], 256)
+    c = CS.Constant("dropout", 0.0)
     d = CS.Constant("global_pool", "add")
     e = CS.CategoricalHyperparameter(
         "conv",
@@ -28,9 +28,9 @@ def get_search_space() -> CS.ConfigurationSpace:
         "PNA",
     )
     # f = CS.CategoricalHyperparameter("heads", [1, 2, 4, 8], 2)
-    g = CS.UniformIntegerHyperparameter("post_layers", 1, 4, 2)
-    h = CS.UniformIntegerHyperparameter("pre_layers", 1, 4, 3)
-    i = CS.CategoricalHyperparameter("towers", [1, 2, 4, 8], 2)
+    g = CS.Constant("post_layers", 1)
+    h = CS.Constant("pre_layers", 1)
+    i = CS.Constant("towers", 1)
     # cond_f = CS.InCondition(f, e, ["GATv2", "Transformer"])
     # cond_g = CS.EqualsCondition(g, e, "PNA")
     # cond_h = CS.EqualsCondition(h, e, "PNA")
