7127175ae77a506d287523d60b9c2a3b5fda15b8
wildsonbbl <wil_bbl@hotmail.com>
2026-09-05 12:30:05 -0300

    clear code

--- gnnepcsaft/configs/search_space.py
+++ gnnepcsaft/configs/search_space.py
@@ -12,21 +12,22 @@ def get_search_space() -> CS.ConfigurationSpace:
     b = CS.CategoricalHyperparameter("hidden_dim", [16, 32, 64, 128, 256, 512], 256)
     c = CS.Constant("dropout", 0.25)
     d = CS.Constant("global_pool", "add")
-    e = CS.CategoricalHyperparameter(
-        "conv",
-        [
-            "PNA",
-            # "GATv2",
-            # "Transformer",
-            # "GCN",
-            # "SAGE",
-            # "GIN",
-            # "GINE",
-            # "Graph",
-            # "SG",
-        ],
-        "PNA",
-    )
+    e = CS.Constant("conv", "PNA")
+    # e = CS.CategoricalHyperparameter(
+    #     "conv",
+    #     [
+    #         "PNA",
+    #         "GATv2",
+    #         "Transformer",
+    #         "GCN",
+    #         "SAGE",
+    #         "GIN",
+    #         "GINE",
+    #         "Graph",
+    #         "SG",
+    #     ],
+    #     "PNA",
+    # )
     # f = CS.CategoricalHyperparameter("heads", [1, 2, 4, 8], 2)
     g = CS.Constant("post_layers", 1)
     h = CS.Constant("pre_layers", 1)
